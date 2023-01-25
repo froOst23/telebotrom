99bf5825361b6800592a3ef187b701ea7fc28cb9
KostiukVadim8 <kostyukvadim8@mail.ru>
2023-01-25 18:30:45 +0500

    БотИК

--- TelebotRom.py
+++ TelebotRom.py
@@ -1,1 +1,20 @@
-print('Hello')
+import os
+
+import telebot
+
+TOKEN = os.environ['TOKEN']
+
+bot = telebot.TeleBot(token=TOKEN)
+
+
+@bot.message_handler(func=lambda message: True)
+def echo(message):
+    bot.reply_to(message, message.text)
+
+
+def main():
+    bot.infinity_polling()
+
+
+if __name__ == '__main__':
+    main()
